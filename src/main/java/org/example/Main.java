6706b56561150f9c439146ece2b8b138e26f91b3
MarcoSkiadas <Skiadas2@hotmail.com>
2024-06-03 15:00:08 +0200

    Adding Student class and main Student update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        List<String> list = new ArrayList();
-        list.add("Hello");
-        list.add("Hey");
-        list.add("World");
 
-        String text = list.get(2);
-        System.out.println(text);
+        Student student1 = new Student("Peter","Pan",1);
+        Student student2 = new Student("Alfredo","Amsel",2);
+        Student student3 = new Student("Bob","Bäcker",3);
+
+        List<Student> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        System.out.println(students);
 
     }
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -5,4 +5,18 @@ public class Student {
     private String lastName;
     private int studentID;
 
+    public Student(String firstName, String lastName, int studentID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentID = studentID;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", studentID=" + studentID +
+                '}';
+    }
 }
